c2b57a4489d7d8f2dcd0cc324bcf3129e7e08de0
Jordan Ball <jball200@caledonian.ac.uk>
2021-02-13 14:08:49 +0000

    First five digits complete

--- routes/licence.py
+++ routes/licence.py
@@ -1,16 +1,18 @@
 from app import app
-from flask import Flask, request, abort
+from flask import Flask, request, abort, make_response, jsonify
 import sys
-from utils import check_allowed_keys
+from utils import check_allowed_keys, get_licence_number, check_datetime_format
+import datetime
 
 @app.route('/licence', methods=['POST'])
 def post_licence():
     post_json = request.get_json()
     allowed_keys = ['first_name', 'middle_name', 'last_name', 'date_of_birth', 'gender']
     check_allowed_keys(allowed_keys, post_json)
-    return "hello"
+    check_datetime_format(post_json)
+    licence_number = get_licence_number(post_json)
+    return jsonify(licence_number)
 
 @app.route('/licence')
 def get_licence():
-    print(request.get_json(), flush=True)
-    return "hello"
+    return make_response(jsonify("OK"), 200)
--- utils.py
+++ utils.py
@@ -1,6 +1,21 @@
 from flask import abort
+import sys
+import datetime
 
 def check_allowed_keys(allowed_keys, passed_in_json):
     for key, value in passed_in_json.items():
         if key not in allowed_keys:
             abort(400, 'Unknown key sent')
+
+def check_datetime_format(post_json):
+    if type(post_json.get('date_of_birth')) == str:
+        try:
+            post_json['date_of_birth'] = datetime.datetime.strptime(post_json.get('date_of_birth'), "%Y-%m-%dT%H:%M:%S")
+            post_json['date_of_birth'] = post_json['date_of_birth'].isoformat()
+        except Exception:
+            abort(400, "Date of birth needs to be in isoformat (1996-02-17T00:00:00)")
+
+def get_licence_number(post_json):
+    #Get first 5 digits
+    last_name = ''.join((post_json.get('last_name'), '99999'))
+    licence_number = last_name[0:5]
